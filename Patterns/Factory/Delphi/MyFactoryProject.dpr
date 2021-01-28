27ce5be5dc523bc95a510174d9b326832bb4d6a3
Molotkov <molotkov@nintegra.ru>
2021-01-28 10:21:49 +0300

    Added factory delphi pattern

---
+++ Patterns/Factory/Delphi/MyFactoryProject.dpr
@@ -0,0 +1,14 @@
+program MyFactoryProject;
+
+uses
+  Forms,
+  MyFactoryUnit in 'MyFactoryUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
